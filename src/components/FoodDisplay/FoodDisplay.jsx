8a41f876db552030c88803cdd3940fe19d9d16dd
tanvirhossain10 <tanvirhossen808@gmail.com>
2024-04-22 17:33:15 +0600

    add food based on user selection on from the menu

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -16,9 +16,17 @@ const FoodDisplay = ({ category }) => {
             <div className="food-display-list">
 
                 {
-                    food_list.map((item, index) => (
-                        <FoodItems key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
-                    ))
+                    food_list.map((item, index) => {
+
+                        if (category === "All" || category === item.category) {
+                            return <FoodItems key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+
+                        }
+                    }
+
+
+
+                    )
                 }
 
             </div>
