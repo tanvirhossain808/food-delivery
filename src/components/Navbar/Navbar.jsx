137b9cc6aa397876070104b08a766e4353d61977
tanvirhossain10 <tanvirhossen808@gmail.com>
2024-05-14 18:56:16 +0600

    complete cart section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,9 @@ const Navbar = ({ setShowLogin }) => {
     const [menu, setMenu] = useState("home")
     return (
         <div className="navbar">
-            <img src={assets.logo} alt="" className="logo" />
+            <Link to="/">
+                <img src={assets.logo} alt="" className="logo" />
+            </Link>
             <ul className="navbar-menu">
                 <Link to="/" onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>home</Link>
                 <a href="#explore-menu" onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</a>
@@ -21,8 +23,11 @@ const Navbar = ({ setShowLogin }) => {
             <div className="navbar-right">
                 <img src={assets.search_icon} alt="" />
                 <div className="navbar-search-icon">
-                    <img src={assets.basket_icon} alt="" />
-                    <div className="dot"></div>
+                    <Link to="/cart">
+                        <img src={assets.basket_icon} alt="" /></Link>
+                    <div className="dot">
+
+                    </div>
                 </div>
                 <button onClick={() => setShowLogin(true)}>sign in</button>
             </div>
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,10 +1,50 @@
+/* eslint-disable no-unused-vars */
+import { useContext } from "react";
 import "./Cart.css";
+import { StoreContext } from "../../context/StoreContent";
 
 const Cart = () => {
-    return (
-        <div>
+    const { cartItems, food_list, removeFromCart } = useContext(StoreContext)
 
-        </div>
+    return (
+        <>
+            <div className="cart">
+                <div className="cart-items">
+                    <div className="cart-items-title">
+                        <p>Items</p>
+                        <p>Title</p>
+                        <p>Price</p>
+                        <p>Quantity</p>
+                        <p>Total</p>
+                        <p>Remove</p>
+                    </div>
+                    <br />
+                    <hr />
+                    {
+                        food_list.map((item, index) => {
+                            if (cartItems[item._id] > 0) {
+                                return (
+                                    <div key={index}>
+                                        <div className="cart-items-title cart-items-item" >
+                                            <img src={item.image} alt="" />
+                                            <p>{item.name}</p>
+                                            <p>${item.price}</p>
+                                            <p>{cartItems[item._id]}</p>
+                                            <p>
+                                                ${cartItems[item._id] * item.price}
+                                            </p>
+                                            <p onClick={() => removeFromCart(item._id)} className="cross">X</p>
+                                        </div>
+                                        <hr />
+                                    </div>
+                                )
+                            }
+                        })
+                    }
+                </div>
+
+            </div >
+        </>
     );
 };
 
