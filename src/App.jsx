eca35832057107554e501c75be62b4c557e95971
tanvirhossain10 <tanvirhossen808@gmail.com>
2024-05-18 15:27:40 +0600

    remove comment for cart page

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
         <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path='/' element={<Home />} />
-          {/* <Route path='/cart' element={<Cart />} /> */}
+          <Route path='/cart' element={<Cart />} />
           <Route path='/order' element={<PlaceOrder />} />
           <Route path='/verify' element={<Verify />} />
           <Route path='/myorders' element={<MyOrders />} />
